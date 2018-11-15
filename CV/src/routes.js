43782e684143cedfa770295b987022b8f80ade02
Mahdi <mahdihsoumi01@gmail.com>
2018-11-15 18:38:49 +0100

    v0.0.1

---
+++ CV/src/routes.js
@@ -0,0 +1,7 @@
+import Home from './components/Home.vue';
+import Portfolio from './components/portfolio/Portfolio.vue';
+
+export const routes = [
+    { path: '/', component: Home},
+    { path: '/Portfolio', component: Portfolio},
+];
